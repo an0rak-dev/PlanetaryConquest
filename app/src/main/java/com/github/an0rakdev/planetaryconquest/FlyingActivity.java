11ffbe68275e478883a73e1557e185bfb18c91a7
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-07-11 17:40:41 +0200

    Add the flying demo activity.

---
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/FlyingActivity.java
@@ -0,0 +1,13 @@
+package com.github.an0rakdev.planetaryconquest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlyingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flying_activity);
+
+    }
+}
