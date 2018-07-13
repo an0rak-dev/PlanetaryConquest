7cb0e5f1d3bc6b0cbc584b8ed0bafa6868d4c3d6
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-07-13 14:54:10 +0200

    Add the Cardboard type to the Android app.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/FlyingActivity.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/FlyingActivity.java
@@ -3,11 +3,20 @@ package com.github.an0rakdev.planetaryconquest;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class FlyingActivity extends Activity {
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+
+public class FlyingActivity extends GvrActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flying_activity);
 
+        GvrView gvrView = findViewById(R.id.flying_gvr_view);
+
+        // Display the view which says the user to put his phone in the Cardboard.
+        gvrView.setTransitionViewEnabled(true);
+
+        setGvrView(gvrView);
     }
 }
