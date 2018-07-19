4ab17fafb5ca82a92f42e67011fc714bae801ee2
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-07-19 10:43:04 +0200

    Refactor : Put the shaders code in dedicated resource files.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/activities/MainActivity.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/activities/MainActivity.java
@@ -1,10 +1,11 @@
-package com.github.an0rakdev.planetaryconquest;
+package com.github.an0rakdev.planetaryconquest.activities;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import com.github.an0rakdev.planetaryconquest.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
