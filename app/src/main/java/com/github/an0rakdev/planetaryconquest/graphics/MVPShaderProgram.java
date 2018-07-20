d519ca38d47ed05a93a6d2f267aa424ebe93686f
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-07-20 14:07:49 +0200

    Rescale shape to a fixed ratio.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/graphics/MVPShaderProgram.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/graphics/MVPShaderProgram.java
@@ -18,6 +18,33 @@ public class MVPShaderProgram extends ShaderProgram {
     @Override
     public void draw(Model shape) {
         GLES20.glUseProgram(this.program);
+        // Apply the model's vertices and the color
+        final int verticesHandle = this.applyVerticesAndColor(shape);
+        // Apply the MVP Matrix
+        final int mvpMatrixHandle = GLES20.glGetUniformLocation(this.program, "vMatrix");
+        float[] mvpMatrix = this.applyModelViewProjection();
+        final float[] scaleMatrix = new float[16];
+        final float[] fullMatrix = new float[16];
+        Matrix.setIdentityM(scaleMatrix, 0);
+        Matrix.setIdentityM(fullMatrix, 0);
+        Matrix.scaleM(scaleMatrix, 0, 0.5f, 0.5f, 0.5f);
+        Matrix.multiplyMM(fullMatrix, 0, mvpMatrix, 0, scaleMatrix, 0);
+        GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false, fullMatrix, 0);
+        // Draw
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, shape.getNbOfVertices());
+        GLES20.glDisableVertexAttribArray(verticesHandle);
+    }
+
+    private float[] applyModelViewProjection() {
+        final float viewMatrix[] = new float[16];
+        final float mvpMatrix[] = new float[16];
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0,-3,
+                0f, 0f, 0f, 1f, 0f, 0f);
+        Matrix.multiplyMM(mvpMatrix, 0, this.projectionMatrix, 0, viewMatrix, 0);
+        return mvpMatrix;
+    }
+
+    private int applyVerticesAndColor(final Model shape) {
         // Add the vertices position to the shader's program.
         final int positionHandle = GLES20.glGetAttribLocation(this.program, "vPosition");
         GLES20.glEnableVertexAttribArray(positionHandle);
@@ -27,20 +54,6 @@ public class MVPShaderProgram extends ShaderProgram {
         // Add the fragments' color to the shader's program.
         final int colorHandle = GLES20.glGetUniformLocation(this.program, "vColor");
         GLES20.glUniform4fv(colorHandle, 1, shape.getFragmentsColor(), 0);
-        // Apply the MVP Matrix
-        this.applyModelViewProjection();
-        // Draw
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, shape.getNbOfVertices());
-        GLES20.glDisableVertexAttribArray(positionHandle);
-    }
-
-    private void applyModelViewProjection() {
-        final int mvpMatrixHandle = GLES20.glGetUniformLocation(this.program, "vMatrix");
-        final float viewMatrix[] = new float[16];
-        final float mvpMatrix[] = new float[16];
-        Matrix.setLookAtM(viewMatrix, 0, 0, 0,-3,
-                0f, 0f, 0f, 1f, 0f, 0f);
-        Matrix.multiplyMM(mvpMatrix, 0, this.projectionMatrix, 0, viewMatrix, 0);
-        GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false, mvpMatrix, 0);
+        return positionHandle;
     }
 }
