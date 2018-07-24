a7088745c23001207baf947846bbd30cba979dcc
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-07-24 12:36:03 +0200

    Display the shape in a VR env

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/graphics/shaders/VRShaderProgram.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/graphics/shaders/VRShaderProgram.java
@@ -9,13 +9,13 @@ import com.github.an0rakdev.planetaryconquest.graphics.models.Model;
 import com.google.vr.sdk.base.Eye;
 
 public class VRShaderProgram extends ShaderProgram {
-    private float[] view;
+    private float[] eyeView;
     private float[] perspective;
 
     public VRShaderProgram(Context context) {
         super(context);
-        this.view = new float[16];
-        Matrix.setIdentityM(this.view, 0);
+        this.eyeView = new float[16];
+        Matrix.setIdentityM(this.eyeView, 0);
         this.perspective = new float[16];
         Matrix.setIdentityM(this.perspective, 0);
         this.addShader(R.raw.mvp_vertex, GLES20.GL_VERTEX_SHADER);
@@ -29,14 +29,36 @@ public class VRShaderProgram extends ShaderProgram {
         float mvpMatrix[] = new float[16];
         Matrix.setIdentityM(mvpMatrix, 0);
 
-        
-        // TODO mvp = this.view * shape.vertices
-        // TODO full = perspective * mvp
-        // TODO draw (based on MVPShaderProgram)
+        final int verticesHandle = GLES20.glGetAttribLocation(this.program, "vPosition");
+        GLES20.glEnableVertexAttribArray(verticesHandle);
+        GLES20.glVertexAttribPointer(verticesHandle, 3, GLES20.GL_FLOAT,
+                false, 3 * shape.getVerticesStride(),
+                shape.getVerticesBuffer());
+        // Add the fragments' color to the shader's program.
+        final int colorHandle = GLES20.glGetUniformLocation(this.program, "vColor");
+        GLES20.glUniform4fv(colorHandle, 1, shape.getFragmentsColor(), 0);
+        // Set the camera (ie. the eye)
+        final float camera[] = new float[16];
+        Matrix.setIdentityM(camera, 0);
+        Matrix.setLookAtM(camera, 0,
+                0.0f, 0.0f, -2f,
+                0.0f, 0.0f, 0.0f,
+                1f, 0.0f, 0.0f);
+        final float realView[] = new float[16];
+        Matrix.setIdentityM(realView, 0);
+        Matrix.multiplyMM(realView, 0, this.eyeView, 0, camera, 0);
+        final float fullMvp[] = new float[16];
+        Matrix.setIdentityM(fullMvp, 0);
+        Matrix.multiplyMM(fullMvp, 0, this.perspective, 0, realView, 0);
+        final int mvpMatrixHandle = GLES20.glGetUniformLocation(this.program, "vMatrix");
+        GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false, fullMvp, 0);
+        // Draw
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, shape.getNbOfVertices());
+        GLES20.glDisableVertexAttribArray(verticesHandle);
     }
 
     public void adaptToEye(final Eye eye) {
-        this.view = eye.getEyeView();
+        this.eyeView = eye.getEyeView();
         this.perspective = eye.getPerspective(0.1f, 100f);
     }
 }
