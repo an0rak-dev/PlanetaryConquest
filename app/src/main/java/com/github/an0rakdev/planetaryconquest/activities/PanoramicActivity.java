9321b1062573f57d1f1888ab1d931d7094a60dfc
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-08-03 14:59:42 +0200

    Display the panoramic picture in the viewer.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/activities/PanoramicActivity.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/activities/PanoramicActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.github.an0rakdev.planetaryconquest.R;
+import com.google.vr.sdk.widgets.common.VrWidgetView;
 import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 
 import java.io.IOException;
@@ -13,20 +14,37 @@ import java.io.InputStream;
 
 public class PanoramicActivity extends AppCompatActivity {
 
+    public static final String TAG = "PanoramicActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.panoramic_actitvity);
+        setContentView(R.layout.panoramic_activity);
         final VrPanoramaView panoBack = findViewById(R.id.pano_view);
+        // Create a panoramic viewer
+        this.loadPanoramaView(panoBack);
+        // Force the VR mode
+        panoBack.setDisplayMode(VrWidgetView.DisplayMode.FULLSCREEN_STEREO);
+    }
 
+    private void loadPanoramaView(VrPanoramaView panoBack) {
+        InputStream is = null;
         try {
-            final InputStream is = getAssets().open("background.jpg");
+            is = getAssets().open("home.jpg");
             VrPanoramaView.Options options = new VrPanoramaView.Options();
-            options.inputType = VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
+            options.inputType = VrPanoramaView.Options.TYPE_MONO;
             panoBack.loadImageFromBitmap(BitmapFactory.decodeStream(is), options);
-            is.close();
+
         } catch (final IOException ex) {
-            Log.e("FlyingActivity", "Error while loading the background.", ex);
+            Log.e(TAG, "Error while loading the background.", ex);
+        } finally {
+            if (null != is){
+                try {
+                    is.close();
+                } catch (final IOException ex) {
+                    Log.e(TAG, "Error while closing the stream to the bg.", ex);
+                }
+            }
         }
     }
 }
