0a3e825e14ac9b7347e3f0714f8402c03af8ea08
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-08-19 23:57:00 +0200

    Create the AsteroidField rendering mechanism.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/demos/renderers/AsteroidsProperties.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/demos/renderers/AsteroidsProperties.java
@@ -3,6 +3,8 @@ package com.github.an0rakdev.planetaryconquest.demos.renderers;
 import android.content.Context;
 import android.util.Log;
 
+import com.github.an0rakdev.planetaryconquest.math.Coordinates;
+
 import java.io.IOException;
 
 /**
@@ -24,5 +26,38 @@ final class AsteroidsProperties extends SpaceProperties {
 		} catch (final IOException ex) {
 			Log.w("AsteroidsProperties", "Unable to load the asteroids configuration.");
 		}
+	}
+
+	/**
+	 * Return the initial camera position in the Asteroids Demo.
+	 *
+	 * @return the intal camera position.
+	 */
+	public Coordinates getCameraPosition() {
+		return new Coordinates(
+				Float.valueOf(this.getProperty("asteroids.camera.position.x", "0")),
+				Float.valueOf(this.getProperty("asteroids.camera.position.y", "0")),
+				Float.valueOf(this.getProperty("asteroids.camera.position.z", "0"))
+		);
+	}
+
+	public int getAsteroidsCount() {
+		return Integer.valueOf(this.getProperty("asteroids.field.count", "0"));
+	}
+
+	public Coordinates getAsteroidsFieldMin() {
+		return new Coordinates(
+				Float.valueOf(this.getProperty("asteroids.field.bound.min.x", "0")),
+				Float.valueOf(this.getProperty("asteroids.field.bound.min.y", "0")),
+				Float.valueOf(this.getProperty("asteroids.field.bound.min.z", "0"))
+		);
+	}
+
+	public Coordinates getAsteroidsFieldMax() {
+		return new Coordinates(
+				Float.valueOf(this.getProperty("asteroids.field.bound.max.x", "0")),
+				Float.valueOf(this.getProperty("asteroids.field.bound.max.y", "0")),
+				Float.valueOf(this.getProperty("asteroids.field.bound.max.z", "0"))
+		);
 	}
 }
--- app/src/main/java/com/github/an0rakdev/planetaryconquest/demos/renderers/AsteroidsRenderer.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/demos/renderers/AsteroidsRenderer.java
@@ -2,6 +2,9 @@ package com.github.an0rakdev.planetaryconquest.demos.renderers;
 
 import android.content.Context;
 
+import com.github.an0rakdev.planetaryconquest.demos.astronomic.AsteroidField;
+import com.github.an0rakdev.planetaryconquest.graphics.models.polyhedrons.Polyhedron;
+import com.github.an0rakdev.planetaryconquest.graphics.shaders.programs.VRProgram;
 import com.google.vr.sdk.base.Eye;
 import com.google.vr.sdk.base.HeadTransform;
 import com.google.vr.sdk.base.Viewport;
@@ -16,6 +19,9 @@ import javax.microedition.khronos.egl.EGLConfig;
  * @version 1.0
  */
 public class AsteroidsRenderer extends SpaceRenderer {
+	private VRProgram vrShader;
+	private AsteroidsProperties config;
+	private AsteroidField field;
 
     /**
      * Create a new Asteroids Renderer with the given Android Context.
@@ -24,11 +30,15 @@ public class AsteroidsRenderer extends SpaceRenderer {
      */
 	public AsteroidsRenderer(final Context context) {
 		super(context, new AsteroidsProperties(context));
+		this.config = new AsteroidsProperties(context);
+		this.field = new AsteroidField(this.config.getAsteroidsCount(),
+				this.config.getAsteroidsFieldMin(), this.config.getAsteroidsFieldMax());
 	}
 
 	@Override
 	public void onSurfaceCreated(final EGLConfig config) {
 		super.onSurfaceCreated(config);
+		this.vrShader = new VRProgram(this.getContext(), this.config.getCameraPosition());
 	}
 
 	@Override
@@ -39,5 +49,9 @@ public class AsteroidsRenderer extends SpaceRenderer {
 	@Override
 	public void onDrawEye(final Eye eye) {
 		super.onDrawEye(eye);
+		this.vrShader.adaptToEye(eye);
+		for (final Polyhedron asteroid : this.field.asteroids()) {
+			this.vrShader.draw(asteroid);
+		}
 	}
 }
