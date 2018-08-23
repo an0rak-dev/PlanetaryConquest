531d531f5945bba34dcbe7ced92cc00cf874acdb
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-08-23 17:44:46 +0200

    Put the GLES20 calls in a specific Util class.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/demos/renderers/AsteroidsRenderer.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/demos/renderers/AsteroidsRenderer.java
@@ -103,34 +103,16 @@ public class AsteroidsRenderer extends SpaceRenderer {
 	public void onDrawEye(final Eye eye) {
 		super.onDrawEye(eye);
 
-		GLES20.glUseProgram(this.celestialProgram);
-
 		Matrix.multiplyMM(this.view, 0, eye.getEyeView(), 0, this.camera, 0);
 		Matrix.multiplyMM(this.modelView, 0, this.view, 0, this.model, 0);
 		Matrix.multiplyMM(this.mvp, 0, eye.getPerspective(0.1f, 100f), 0, this.view, 0);
-		// Pass the MVP to the shader.
-		final int mvpHandle = GLES20.glGetUniformLocation(this.celestialProgram, "vMatrix");
-		GLES20.glUniformMatrix4fv(mvpHandle, 1, false, this.mvp, 0);
+		OpenGLUtils.use(this.celestialProgram);
+		OpenGLUtils.bindMVPToProgram(this.celestialProgram, this.mvp, "vMatrix");
 
 		for (final Polyhedron asteroid : this.field.asteroids()) {
-			draw(asteroid);
+			final int verticesHandle = OpenGLUtils.bindVerticesToProgram(this.celestialProgram, asteroid.bufferize(), "vVertices");
+			final int colorHandle = OpenGLUtils.bindColorToProgram(this.celestialProgram, asteroid.colors(), "vColors");
+			OpenGLUtils.drawTriangles(asteroid.size(), verticesHandle, colorHandle);
 		}
-	}
-
-
-	private void draw(final Polyhedron sphere)  {
-		// Pass the vertices to the shader.
-		final int verticesHandle = GLES20.glGetAttribLocation(this.celestialProgram, "vVertices");
-		GLES20.glEnableVertexAttribArray(verticesHandle);
-		GLES20.glVertexAttribPointer(verticesHandle, DIMENSION, GLES20.GL_FLOAT, false, 3* FLOAT_BYTES, sphere.bufferize());
-		// Pass the color to the shader.
-		final int colorHandle = GLES20.glGetAttribLocation(this.celestialProgram, "vColors");
-		GLES20.glEnableVertexAttribArray(colorHandle);
-		GLES20.glVertexAttribPointer(colorHandle, 4, GLES20.GL_FLOAT, false, 0, sphere.colors());
-
-		// Draw.
-		GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, sphere.size());
-		GLES20.glDisableVertexAttribArray(colorHandle);
-		GLES20.glDisableVertexAttribArray(verticesHandle);
 	}
 }
