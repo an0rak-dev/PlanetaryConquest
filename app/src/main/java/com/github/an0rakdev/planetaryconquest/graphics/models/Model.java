9f7134aa9b1addabc94ea42d30f24b442a7c322b
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-08-30 10:51:18 +0200

    Take the camera movement in account when destroying asteroids

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/graphics/models/Model.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/graphics/models/Model.java
@@ -13,6 +13,7 @@ import java.nio.FloatBuffer;
 public abstract class Model {
 	/** The size of a Float in Bytes. */
 	public static final int FLOAT_BYTE_SIZE = (Float.SIZE) / Byte.SIZE;
+    public static final int COLOR_SIZE = 4;
 
     /**
 	 * Returns the buffer of vertices coordinates for the model.
@@ -29,6 +30,18 @@ public abstract class Model {
      * @return the number of vertices used for drawing this model.
      */
     public abstract int size();
+
+    /**
+     * Create and return a float buffer which contains the colors of each vertices in
+     * the same order as <code>bufferize</code>.
+     *
+     * A background is represented has 4 consecutive values in the resulting buffers (rgba)
+     * so the total size of this buffer will be :
+     * <code>this.getSize() * 4 * FLOAT_BYTE_SIZE</code>.
+     *
+     * @return the bufferized colors to apply to this model.
+     */
+    public abstract FloatBuffer colors();
 
     protected FloatBuffer createFloatBuffer(final int size) {
         final ByteBuffer bb = ByteBuffer.allocateDirect(size);
