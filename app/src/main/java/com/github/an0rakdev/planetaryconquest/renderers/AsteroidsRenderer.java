28c03458da39c3f93beace570e8331da57849f2d
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-09-12 16:39:32 +0200

    Create only once the matrix used for rendering the lasers.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/renderers/AsteroidsRenderer.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/renderers/AsteroidsRenderer.java
@@ -53,12 +53,17 @@ public class AsteroidsRenderer extends SpaceRenderer {
 	private FloatBuffer crossVertices;
 	private final float[] headView;
 
+	private final float[] currentAsteroidModel;
 	private List<Laser> lasers;
 	private final float laserSpeed;
 	private int lasersProgram;
 	private long currentCooldown;
+	private final float[] lasersCamera;
+	private final float[] lasersView;
+	private final float[] lasersMvp;
 
 	private final GvrAudioEngine audioEngine;
+	private final float[] headQuaternion;
 
     /**
      * Create a new Asteroids Renderer with the given Android Context.
@@ -83,10 +88,15 @@ public class AsteroidsRenderer extends SpaceRenderer {
 		this.modelHud = new float[16];
 		this.hudCamera = new float[16];
 		this.headView = new float[16];
+		this.currentAsteroidModel = new float[16];
 		this.lasers = new ArrayList<>();
 		this.laserSpeed = config.getLaserSpeed() / 1000;
 		this.currentCooldown = 0L;
+		this.lasersCamera = new float[16];
+		this.lasersView = new float[16];
+		this.lasersMvp = new float[16];
 		this.audioEngine = new GvrAudioEngine(context, GvrAudioEngine.RenderingMode.BINAURAL_HIGH_QUALITY);
+		this.headQuaternion = new float[4];
 		initializeAsteroidField(config);
 		initializeHud(config);
 	}
@@ -111,7 +121,10 @@ public class AsteroidsRenderer extends SpaceRenderer {
 		final int lasersVertexShader = OpenGLUtils.addVertexShaderToProgram(vertexSources, this.lasersProgram);
 		final int lasersFragmentShader = OpenGLUtils.addFragmentShaderToProgram(fragmentSources, this.lasersProgram);
 		OpenGLUtils.linkProgram(this.lasersProgram, lasersVertexShader, lasersFragmentShader);
-
+		Matrix.setLookAtM(lasersCamera, 0,
+				getProperties().getCameraPositionX(), getProperties().getCameraPositionY(), getProperties().getCameraPositionZ(),
+				getProperties().getCameraDirectionX(), getProperties().getCameraDirectionY(), getProperties().getCameraDirectionZ(),
+				0, 1, 0);
 
         new Thread(new Runnable() {
                     @Override
@@ -134,21 +147,21 @@ public class AsteroidsRenderer extends SpaceRenderer {
 		final float cameraDirX = getProperties().getCameraDirectionX();
 		final float cameraDirY = getProperties().getCameraDirectionY();
 		final float cameraDirZ = getProperties().getCameraDirectionZ();
-/*
+
 		this.asteroidMvt += asteroidsDistance;
 		this.cameraPosX += asteroidsDistance;
 		if (this.cameraMvt < ((AsteroidsProperties) getProperties()).getDistanceToTravel()) {
-			cameraPosZ += cameraDistance;
-			this.cameraMvt += cameraDistance;
+	//		cameraPosZ += cameraDistance;
+	//		this.cameraMvt += cameraDistance;
 		}
-*/
+
 		for (final Laser laser : this.lasers) {
 			laser.move(0,0,laserDistance);
 		}
 
 		this.currentCooldown -= time;
 
-	//	checkCollisions();
+		checkCollisions();
 
 		Matrix.setLookAtM(this.camera, 0,
 				cameraPosX, cameraPosY, cameraPosZ,
@@ -161,10 +174,9 @@ public class AsteroidsRenderer extends SpaceRenderer {
 
 		headTransform.getHeadView(this.headView, 0);
 
-		final float[] quaternion = new float[4];
-		headTransform.getQuaternion(quaternion, 0);
-		audioEngine.setHeadRotation(quaternion[0], quaternion[1],
-                quaternion[2], quaternion[3]);
+		headTransform.getQuaternion(this.headQuaternion, 0);
+		audioEngine.setHeadRotation(this.headQuaternion[0], this.headQuaternion[1],
+				this.headQuaternion[2], this.headQuaternion[3]);
 		audioEngine.update();
 	}
 
@@ -177,10 +189,9 @@ public class AsteroidsRenderer extends SpaceRenderer {
 		OpenGLUtils.use(this.celestialProgram);
 		OpenGLUtils.bindMVPToProgram(this.celestialProgram, this.mvp, "vMatrix");
 
-		for (int i=0; i < this.field.size(); i++) {
-			final Sphere asteroid = this.field.get(i);
+		for (final Sphere asteroid : this.field) {
             if (isLookingAt(asteroid) && 0L >= this.currentCooldown) {
-				fireAt(i);
+				fireAt(asteroid);
 			}
 			final int verticesHandle = OpenGLUtils.bindVerticesToProgram(this.celestialProgram, asteroid.bufferize(), "vVertices");
 			final int colorHandle = OpenGLUtils.bindColorToProgram(this.celestialProgram, asteroid.colors(), "vColors");
@@ -204,7 +215,7 @@ public class AsteroidsRenderer extends SpaceRenderer {
 		final float maxX = config.getAsteroidMaxX();
 		final float maxY = config.getAsteroidMaxY();
 		final float maxZ = config.getAsteroidMaxZ();
-/*
+
 		while (this.field.size() < asteroidsCount) {
 			final float radius = MathUtils.randRange(minSize, maxSize);
 			final Coordinates center = new Coordinates(
@@ -216,11 +227,6 @@ public class AsteroidsRenderer extends SpaceRenderer {
 			asteroid.background(asteroidColor);
 			this.field.add(asteroid);
 		}
-		*/
-		final Sphere asteroid = new Sphere(new Coordinates(1, 1, 6), 1);
-		asteroid.precision(1);
-		asteroid.background(asteroidColor);
-		this.field.add(asteroid);
 	}
 
 	public void pause() {
@@ -284,32 +290,23 @@ public class AsteroidsRenderer extends SpaceRenderer {
 	}
 
 	private void displayLasers(final Eye eye) {
-		final float[] lasersCamera = new float[16];
-		final float[] lasersView = new float[16];
-		final float[] lasersMvp = new float[16];
-		final float[] laserModel = new float[16];
+		float[] laserModel = null;
 		final float[] laserModelView = new float[16];
 
-		Matrix.setLookAtM(lasersCamera, 0,
-				getProperties().getCameraPositionX(), getProperties().getCameraPositionY(), getProperties().getCameraPositionZ(),
-				getProperties().getCameraDirectionX(), getProperties().getCameraDirectionY(), getProperties().getCameraDirectionZ(),
-				0, 1, 0);
-		Matrix.multiplyMM(lasersView, 0, eye.getEyeView(), 0, lasersCamera, 0);
+
+		Matrix.multiplyMM(this.lasersView, 0, eye.getEyeView(), 0, this.lasersCamera, 0);
 		OpenGLUtils.use(this.lasersProgram);
 
 		final List<Laser> oldLasers = new ArrayList<>();
 		for (final Laser laser : this.lasers) {
-			Matrix.setIdentityM(laserModel, 0);
-			Matrix.translateM(laserModel, 0, laser.getPosition().x, laser.getPosition().y, laser.getPosition().z);
-			Matrix.multiplyMM(laserModel, 0, laser.translations(), 0, laserModel, 0);
-			Matrix.multiplyMM(laserModel, 0, laser.rotation(), 0, laserModel, 0);
+			laserModel = laser.model();
 			if (laserModel[14] > 30) {
 				oldLasers.add(laser);
 				this.audioEngine.stopSound(laser.audio());
 			} else {
-				Matrix.multiplyMM(laserModelView, 0, lasersView, 0, laserModel, 0);
-				Matrix.multiplyMM(lasersMvp, 0, eye.getPerspective(0.1f, 100f), 0, laserModelView, 0);
-				OpenGLUtils.bindMVPToProgram(this.lasersProgram, lasersMvp, "vMatrix");
+				Matrix.multiplyMM(laserModelView, 0, this.lasersView, 0, laserModel, 0);
+				Matrix.multiplyMM(this.lasersMvp, 0, eye.getPerspective(0.1f, 100f), 0, laserModelView, 0);
+				OpenGLUtils.bindMVPToProgram(this.lasersProgram, this.lasersMvp, "vMatrix");
 				final int verticesHandle = OpenGLUtils.bindVerticesToProgram(this.lasersProgram, laser.bufferize(), "vVertices");
 				final int colorHandle = OpenGLUtils.bindColorToProgram(this.lasersProgram, laser.colors(), "vColors");
 				OpenGLUtils.drawLines(laser.size(), 120, verticesHandle, colorHandle);
@@ -321,31 +318,22 @@ public class AsteroidsRenderer extends SpaceRenderer {
 	}
 
 	private boolean isLookingAt(final Sphere shape) {
-		final float[] asteroidModel = new float[16];
-		Matrix.setIdentityM(asteroidModel, 0);
+		Matrix.setIdentityM(this.currentAsteroidModel, 0);
 		final float realXPos = shape.getPosition().x - this.asteroidMvt;
 		final float realZPos = shape.getPosition().z - this.cameraMvt;
-		Matrix.translateM(asteroidModel, 0, realXPos, shape.getPosition().y, realZPos);
+		Matrix.translateM(this.currentAsteroidModel, 0, realXPos, shape.getPosition().y, realZPos);
 		final float[] modelView = new float[16];
-		Matrix.multiplyMM(modelView, 0, this.headView, 0, asteroidModel, 0);
-
+		Matrix.multiplyMM(modelView, 0, this.headView, 0, this.currentAsteroidModel, 0);
 		// shape Area
 		final float shapeAreaRad = (float) Math.atan2(shape.getRadius(), realZPos);
-		final float shapeAreaDeg = (float) Math.toDegrees(shapeAreaRad);
-
 		// pitch of sight
 		final float sightPitchRad = (float) Math.atan2(modelView[12], modelView[14]);
-		final float sightPitchDeg = (float) Math.toDegrees(sightPitchRad);
-
 		// yaw of sight
 		final float sightYawRad = (float) Math.atan2(modelView[13], modelView[14]);
-		final float sightYawDeg = (float) Math.toDegrees(sightYawRad);
-
-		return Math.abs(sightPitchDeg) < shapeAreaDeg && Math.abs(sightYawDeg) < shapeAreaDeg;
+		return Math.abs(sightPitchRad) < shapeAreaRad && Math.abs(sightYawRad) < shapeAreaRad;
 	}
 
-	private void fireAt(final int asteroidIdx) {
-		final Sphere target = this.field.get(asteroidIdx);
+	private void fireAt(final Sphere target) {
 		final Coordinates start = new Coordinates(0, -1, this.cameraPosZ - 1);
 		final Coordinates end = new Coordinates(0, -1, start.z + 0.3f);
 		final Laser laser = new Laser(start, end);
@@ -354,9 +342,7 @@ public class AsteroidsRenderer extends SpaceRenderer {
 		// pitch ( polar angle)
 		final float realXPos = target.getPosition().x - this.asteroidMvt;
 		final float realZPos = target.getPosition().z - this.cameraMvt;
-		final float xSquare = realXPos * realXPos;
-		final float ySquare = target.getPosition().y * target.getPosition().y;
-		final float angleInRadian = (float) Math.atan2(Math.sqrt(xSquare+ySquare),realZPos);
+		final float angleInRadian = (float) Math.atan2(realXPos,realZPos);
 		laser.pitch((float)Math.toDegrees(angleInRadian));
 
 		// yaw
@@ -389,7 +375,7 @@ public class AsteroidsRenderer extends SpaceRenderer {
 					this.audioEngine.setSoundObjectPosition(explosionSound,
 							asteroid.getPosition().x, asteroid.getPosition().y, asteroid.getPosition().z);
 					this.audioEngine.playSound(explosionSound, false);
-					continue;
+					break;
 				}
 			}
 			this.field.removeAll(asteroidsToRemove);
@@ -402,21 +388,10 @@ public class AsteroidsRenderer extends SpaceRenderer {
 		final float shapeY = shape.getPosition().y;
 		final float shapeZ = shape.getPosition().z - this.cameraMvt;
 		final float rad = shape.getRadius();
-
-
-		final float[] laserModel = new float[16];
-		final float[] laserRealPos = new float[4];
-		Matrix.setIdentityM(laserModel, 0);
-		Matrix.translateM(laserModel, 0, laser.getPosition().x, laser.getPosition().y, laser.getPosition().z);
-		Matrix.multiplyMM(laserModel, 0, laser.translations(), 0, laserModel, 0);
-		Matrix.multiplyMM(laserModel, 0, laser.rotation(), 0, laserModel, 0);
-		Matrix.multiplyMV(laserRealPos, 0, laserModel, 0, new float[]{0,0,0,1}, 0);
-
-
-		boolean result = shapeX - rad < laserRealPos[0] && laserRealPos[0] <= shapeX + rad;
-		result = result && shapeY - rad < laserRealPos[1] && laserRealPos[1] <= shapeY + rad;
-		result = result && shapeZ - rad < laserRealPos[2] && laserRealPos[2] <= shapeZ + rad;
-
+		final float[] laserModel = laser.model();
+		boolean result = shapeX - rad < laserModel[12] && laserModel[12] <= shapeX + rad;
+		result = result && shapeY - rad < laserModel[13] && laserModel[13] <= shapeY + rad;
+		result = result && shapeZ - rad < laserModel[14] && laserModel[14] <= shapeZ + rad;
 		return result;
 	}
 }
