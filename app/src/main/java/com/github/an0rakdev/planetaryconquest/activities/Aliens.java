d29102d14a101c02c782e90280d0005c0b5cc412
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-09-16 15:44:34 +0200

    Add an arrival sound for each ship.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/activities/Aliens.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/activities/Aliens.java
@@ -10,16 +10,29 @@ import com.google.vr.sdk.base.GvrActivity;
 import com.google.vr.sdk.base.GvrView;
 
 public class Aliens extends GvrActivity {
+    private AliensRenderer renderer;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.asteroids_activity);
         final GvrView gvrView = findViewById(R.id.asteroids_gvr_view);
-        SpaceRenderer renderer = new AliensRenderer(this);
-        gvrView.setRenderer(renderer);
+        this.renderer = new AliensRenderer(this);
+        gvrView.setRenderer(this.renderer);
         gvrView.setTransitionViewEnabled(true);
         this.setGvrView(gvrView);
+    }
+
+    @Override
+    public void onPause() {
+        this.renderer.pause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        this.renderer.resume();
+        super.onResume();
     }
 
 }
