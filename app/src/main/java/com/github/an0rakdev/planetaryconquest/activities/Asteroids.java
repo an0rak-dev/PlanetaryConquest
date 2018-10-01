eabacf72bc691a103e7b648a7e196e267554d997
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-10-01 18:00:49 +0200

    Remove any use of properties in the Asteroids demo.

--- app/src/main/java/com/github/an0rakdev/planetaryconquest/activities/Asteroids.java
+++ app/src/main/java/com/github/an0rakdev/planetaryconquest/activities/Asteroids.java
@@ -11,17 +11,6 @@ public class Asteroids extends GvrActivity {
 	private AsteroidsRenderer renderer;
 
 	@Override
-	protected void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.setContentView(R.layout.asteroids_activity);
-		final GvrView gvrView = findViewById(R.id.asteroids_gvr_view);
-		this.renderer = new AsteroidsRenderer(this);
-		gvrView.setRenderer(this.renderer);
-		gvrView.setTransitionViewEnabled(true);
-		this.setGvrView(gvrView);
-	}
-
-	@Override
 	public void onPause() {
 		this.renderer.pause();
 		super.onPause();
@@ -31,5 +20,15 @@ public class Asteroids extends GvrActivity {
 	public void onResume() {
 		this.renderer.resume();
 		super.onResume();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.asteroids_activity);
+		final GvrView gvrView = findViewById(R.id.asteroids_gvr_view);
+		this.renderer = new AsteroidsRenderer(this);
+		gvrView.setRenderer(this.renderer);
+		this.setGvrView(gvrView);
 	}
 }
